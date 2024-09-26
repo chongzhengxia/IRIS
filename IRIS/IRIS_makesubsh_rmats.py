a0eac5aa1388fba037357dcef5c6b6884d2a440c
chongzhengxia <chongzhengxia@126.com>
2024-09-26 15:28:13 +0800

    Update IRIS_makesubsh_rmats.py

--- IRIS/IRIS_makesubsh_rmats.py
+++ IRIS/IRIS_makesubsh_rmats.py
@@ -1,9 +1,10 @@
 import sys, csv, glob, os
 from . import config
-
-def writeShell(rMATS_path, fin_name, folder_name, bam_dir, read_length_argument, gtf, novelSS, task_name, task_dir):
-	fout_local=open(folder_name+'/bam_list.txt','w')
-	fout_local.write(fin_name)
+                         # writeShell(rMATS_path, fin_name, folder_name,          bam_dir, str(read_length), gtf, novelSS, task_name, task_dir)
+            # rMATS程序路径       bam文件绝对路径  bam文件dirname    一级目录   
+def writeShell(rMATS_path,              fin_name,    folder_name,     bam_dir, read_length_argument, gtf, novelSS, task_name, task_dir):
+	fout_local=open(folder_name+'/bam_list.txt','w')  # 在bam文件的同一级目录中打开个文件
+	fout_local.write(fin_name)                        # 在bam文件的同一级目录写入bam文件的绝对路径到一个文件里
 	fout_local.close()
 
 	sample_name=folder_name.split('/')[-1].split('.')[0]
@@ -20,7 +21,7 @@ def writeShell(rMATS_path, fin_name, folder_name, bam_dir, read_length_argument,
 	# A future version of rMATS may fix this behavior.
 	fout.write('python {} --b1 {}/bam_list.txt --od {} --tmp {}/{}.RL{}/{}.tmp --anchorLength 1 --readLength {} --gtf {} -t paired --task prep --nthread 8 --statoff {}|| true\n'.format(rMATS_path, folder_name, folder_name, bam_dir, task_name, read_length_argument, sample_name, read_length_argument, gtf, novelSS_str))
 	fout.close()
-
+        # 每一个bam文件运行prep关闭statoff统计信息
 def organizeReadLength(rMATS_path, file_list_mapping, gtf, novelSS, bam_prefix, task_name, task_dir):
 	rl_dict={}
 	folder_names={}
@@ -40,27 +41,27 @@ def organizeReadLength(rMATS_path, file_list_mapping, gtf, novelSS, bam_prefix, 
 def main(args):
 	gtf=args.gtf
 	task_name=args.data_name
-	rMATS_path=args.rMATS_path
-	bam_dir= args.bam_dir.rstrip('/')
-	bam_prefix=args.bam_prefix
-	novelSS=args.novelSS
-	task_dir=args.task_dir
+	rMATS_path=args.rMATS_path         # rMATS的代码的路径
+	bam_dir= args.bam_dir.rstrip('/')  # process_rna 的一级输出目录，二级目录是star的输出目录，是process_rna的 -p参数 --sampleID-outdir
+	bam_prefix=args.bam_prefix         # bam文件的前缀，一般是Aligned.sortedByCoord.out
+	novelSS=args.novelSS               # 是否开启rMATS 的novelSS选项，一般是关闭 
+	task_dir=args.task_dir             # 任务脚本目录的路径
 	if not os.path.exists(task_dir):
 		os.makedirs(task_dir)
-	if args.read_length:
+	if args.read_length:              # read_length的设置，一般是从star的输出结果里获取
 		read_length= int(args.read_length)
 	
 	print 'preparing rMATS-turbo prep directories'
 	if args.read_length==False:
-		mapping_log_file_list=glob.glob(bam_dir+'/*/Log.final.out')
+		mapping_log_file_list=glob.glob(bam_dir+'/*/Log.final.out')   # 找到star的输出结果从而获取read_length，应该是有个bug 应该加上Log.final.out的前缀
 		organizeReadLength(rMATS_path, mapping_log_file_list, gtf, novelSS, bam_prefix, task_name, task_dir) #relocated based on the read length
 	else:
-		mapping_bam_list=glob.glob(bam_dir+'/*/'+bam_prefix+'.bam')
+		mapping_bam_list=glob.glob(bam_dir+'/*/'+bam_prefix+'.bam')  # 包含bam文件路径的列表
 		os.system('mkdir -p '+bam_dir+'/'+task_name+'.RL'+str(read_length))
 		for fin_name in mapping_bam_list:
-			folder_name= '/'.join(fin_name.split('/')[:-1])
-			writeShell(rMATS_path, fin_name, folder_name, bam_dir, str(read_length), gtf, novelSS, task_name, task_dir)
-
-
+			folder_name= '/'.join(fin_name.split('/')[:-1]) #bam文件的dirname
+			writeShell(rMATS_path, fin_name, folder_name,          bam_dir, str(read_length), gtf, novelSS, task_name, task_dir)
+                         # rMATS程序路径       bam文件绝对路径  bam文件dirname    一级目录    
+ 
 if __name__ == '__main__':
 	main()
