198d57a1f44449feba8e5aeb1c482fb8949ebc8b
chongzhengxia <chongzhengxia@126.com>
2024-09-30 13:05:24 +0800

    Update IRIS_makesubsh_rmatspost.py

--- IRIS/IRIS_makesubsh_rmatspost.py
+++ IRIS/IRIS_makesubsh_rmatspost.py
@@ -1,14 +1,15 @@
 import sys,glob,os,argparse
 from . import config
-                          #      star_output1的绝对路径
+                          #      task_name.RL150
 def write_task_script(rMATS_path, bam_dir, task_name, gtf, novelSS, task_dir):
-	# 这里的bam_dir是star的输出目录
-	read_length=int(bam_dir.split('/')[-1].split('.')[-1][2:])
-	dir_name=task_name+'_RL'+str(read_length)
+	#                task_name.RL150
+	read_length=int(bam_dir.split('/')[-1].split('.')[-1][2:])   # 150
+	dir_name=task_name+'_RL'+str(read_length)                    # task_name_RL150
 	graphlist=glob.glob(bam_dir+'/*.tmp/*')
 	os.system('mkdir -p '+bam_dir+'/'+dir_name+'.graph')
 	os.system('cp '+bam_dir+'/*.tmp/* '+bam_dir+'/'+dir_name+'.graph/.')   # 将task_name.RL150/.tmp文件夹下的所有文件移动到 task_name.RL150/task_name_RL150.graph下
 	print '[INFO] Done copy'
+	                        task_name_RL150
 	cmd='head -n1 -q '+bam_dir+'/'+dir_name+'.graph/*.rmats |paste -d, -s >'+bam_dir+'/'+dir_name+'_rmatspost_list.txt'
 	print cmd
 	os.system(cmd)
@@ -24,7 +25,9 @@ def write_task_script(rMATS_path, bam_dir, task_name, gtf, novelSS, task_dir):
 	# failure return code from the python command.
 	# rMATS produces the desired output files despite the error return.
 	# A future version of rMATS may fix this behavior.
+									# task_name_RL150_rmatspost_list.txt
 	fout.write('#!/bin/bash\npython '+rMATS_path+' --b1 '+bam_dir+'/'+dir_name+'_rmatspost_list.txt --od '+bam_dir+'/'+dir_name+'.matrix --tmp '+bam_dir+'/'+dir_name+'.graph/ --anchorLength 1 --readLength '+str(read_length)+' --gtf '+gtf+' -t paired --nthread 8 --task post --statoff '+novelSS_str+'|| true\n')
+		                                                                            # task_name.RL150              task_name_RL150.matrix
 	fout.close()
 	return 
 
@@ -39,7 +42,8 @@ def main(args):
 	if not os.path.exists(task_dir):
 		os.makedirs(task_dir)
 
-	rl_bam_folders=glob.glob(prep_dir+'/'+task_name+'.RL*')
+	rl_bam_folders=glob.glob(prep_dir+'/'+task_name+'.RL*')  # 找到全部的rmats prep步骤的输出tmp目录的高一级目录
+	#   task_name.RL150
 	for bam_folder in rl_bam_folders:
 		print bam_folder
 		write_task_script(rMATS_path, bam_folder,task_name, gtf, novelSS, task_dir)
